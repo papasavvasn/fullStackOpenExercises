99db4f2fe0d1e0930496d73e8964ffb70bad021f
papasavvasn <papasavvasn@media-saturn.com>
2020-07-04 13:44:42 +0200

    feat(part2forms): complete exercise 2.6, Phonebook step1

---
+++ exercises/part2/phonebook/src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useState, FormEvent } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  function addName(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    setPersons([...persons, { name: newName }])
+  }
+
+
+  function handleChange(event: { target: HTMLInputElement }) {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App
