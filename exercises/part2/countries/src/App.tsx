d65c678b64d90c45cc083c754e951aa049a9b31e
papasavvasn <papasavvasn@media-saturn.com>
2020-07-06 23:04:23 +0200

    feat(part2c): finish ex 2.13, countries step2

--- exercises/part2/countries/src/App.tsx
+++ exercises/part2/countries/src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios"
 import { Filter } from './Filter'
-import { Countries, CountryProps } from './Countries'
+import { Countries, CountryType } from './Countries'
 
 
 const App = () => {
 
-  const [countries, setCountries] = useState<CountryProps[]>([])
+  const [countries, setCountries] = useState<CountryType[]>([])
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
--- exercises/part2/countries/src/Countries.tsx
+++ exercises/part2/countries/src/Countries.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export type CountryProps = {
+export type CountryType = {
     name: string;
     capital: string;
     population: number;
@@ -8,7 +8,7 @@ export type CountryProps = {
     flag: string;
 }
 
-const Country = ({ name, capital, population, languages, flag }: CountryProps) => <>
+const Country = ({ name, capital, population, languages, flag }: CountryType) => <>
     <h2>{name}</h2>
     <br />
     capital {capital}<br />
@@ -21,14 +21,18 @@ const Country = ({ name, capital, population, languages, flag }: CountryProps) =
     <img style={{ marginTop: "1.5em", width: "6em" }} alt={`flag of ${name}`} src={flag} />
 </>
 
-export const Countries = ({ countries, filter }: { countries: CountryProps[], filter: string }) => {
+export const Countries = ({ countries, filter }: { countries: CountryType[], filter: string }) => {
 
+    const [selectedCountry, setSelectedCountry] = useState<CountryType>()
     const matches = countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase()))
 
+    if (selectedCountry) {
+        return <Country {...selectedCountry} />
+    }
     if (matches.length === 1) {
         return <Country {...matches[0]} />
     } else if (matches.length <= 10) {
-        return <>{matches.map(country => <p key={country.name}>{country.name}</p>)}</>
+        return <>{matches.map(country => <p key={country.name}>{country.name}<button onClick={() => setSelectedCountry(country)}>show</button> </p>)}</>
     } else if (matches.length > 10 && filter.length > 0) {
         return <p>Too many matches, specify another filter</p>
     } return null
