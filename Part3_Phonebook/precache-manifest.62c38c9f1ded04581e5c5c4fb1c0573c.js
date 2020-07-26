e6960db1e58a0f094f0d2b038dcc10004bad7a7e
papasavvasn <papasavvasn@media-saturn.com>
2020-07-26 13:30:09 +0200

    configure client to be served from my server

---
+++ Part3_Phonebook/precache-manifest.62c38c9f1ded04581e5c5c4fb1c0573c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ee787d5a2896ba46bd2a12c2caa5c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e208d48540877d583bb5",
+    "url": "/static/css/main.b62864b7.chunk.css"
+  },
+  {
+    "revision": "63b85a6b3bd9f3881dee",
+    "url": "/static/js/2.ee45aa4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ee45aa4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e208d48540877d583bb5",
+    "url": "/static/js/main.66278ebf.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- exercises/part2/phonebook/src/services/persons.ts
+++ exercises/part2/phonebook/src/services/persons.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import { Person } from '../Persons'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getPersons = () => axios.get(baseUrl)
 
-const addPerson = ({ name, number, id }: Person) => axios.post('http://localhost:3001/persons', { name, number, id })
+const addPerson = ({ name, number, id }: Person) => axios.post(baseUrl, { name, number, id })
 
 const deletePerson = (id: string) => axios.delete(`${baseUrl}/${id}`)
 
